4911bbc802e4ee2da564b5d28d810903d70fcf44
1dam <1dam@hz319579.TartangaLH.eus>
2019-02-12 14:20:22 +0100

    Mi primer commit

---
+++ Calculadora/src/CalculadoraPack/Calculadora.java
@@ -0,0 +1,46 @@
+package CalculadoraPack;
+
+public class Calculadora {
+	private int n1;
+	private int n2;
+	
+	public Calculadora (int a, int b)
+	{
+		n1 = a;
+		n2 = b;
+	}
+	
+	public int suma()
+	{
+		int result = n1 + n2;
+		return result;
+	}
+	public int resta()
+	{
+		int result;
+		if(resta2()) result = n1 - n2;
+		else result = n2 - n1;
+		return result;
+	}
+	public int multiplica()
+	{
+		int result = n1 * n2;
+		return result;
+	}
+	public int divide()
+	{
+		int result = n1 / n2;
+		return result;
+	}
+	public boolean resta2() 
+	{
+		if (n1 >= n2) return true;
+		else return false;
+	}
+	public Integer divide2() 
+	{
+		if(n2==0) return null;
+		int result = n1 / n2;
+		return (result);
+	}
+}
---
+++ Calculadora/src/CalculadoraPack/CalculadoraTest.java
@@ -0,0 +1,54 @@
+package CalculadoraPack;
+
+import static org.junit.Assert.*;
+
+import javax.swing.SingleSelectionModel;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+	@Test
+	public void testSuma() {
+		Calculadora calcu = new Calculadora(20, 10);
+		int resultado = calcu.suma();
+		assertEquals(30, resultado);
+	}
+
+	@Test
+	public void testResta() {
+		Calculadora calcu = new Calculadora(15, 5);
+		int resultado = calcu.resta();
+		assertEquals(10, resultado);
+	}
+
+	@Test
+	public void testMultiplica() {
+		Calculadora calcu = new Calculadora(5, 6);
+		int resultado = calcu.multiplica();
+		assertEquals(30, resultado);
+	}
+
+	@Test
+	public void testDivide() {
+		Calculadora calcu = new Calculadora(50, 10);
+		int resultado = calcu.divide();
+		assertEquals(5, resultado);
+	}
+	
+	@Test
+	public void testDivideExcept() {
+		try {
+			Calculadora calcu = new Calculadora (20, 0);
+			int resultado = calcu.divide();
+			assertEquals(10,resultado);
+			fail("Fallo, Debería haber lanzado la xcepción");
+		}
+		catch (ArithmeticException e) {
+			System.out.println("PRUEBA satisfactoria");
+		}
+		
+	
+	}
+
+}
