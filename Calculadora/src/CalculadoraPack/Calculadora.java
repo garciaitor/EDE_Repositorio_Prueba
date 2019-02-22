874541a4a17fbadcb8a1af3e1fe47f4b7d3fd121
1dam <1dam@HZ301506.TartangaLH.eus>
2019-02-22 09:30:30 +0100

    aaaaa

--- Calculadora/src/CalculadoraPack/Calculadora.java
+++ Calculadora/src/CalculadoraPack/Calculadora.java
@@ -46,6 +46,7 @@ public class Calculadora {
 		if(n2==0) return null;
 		int result = n1 / n2;
 		System.out.println("Division");
+		System.out.println("dfjo");
 		return (result);
 	}
 	public void upperhola() {
